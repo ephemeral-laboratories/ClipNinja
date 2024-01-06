6a9ba4b9cf81c07efeba324633d089e68775e66c
Hakanai <hakanai@ephemeral.garden>
2024-01-07 08:53:23 +1100

    Removing unused import

--- src/jvmMain/kotlin/HistoryScreen.kt
+++ src/jvmMain/kotlin/HistoryScreen.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@file:OptIn(ExperimentalFoundationApi::class)
 
 package garden.ephemeral.clipboard.ninja
 
@@ -15,7 +15,6 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toComposeImageBitmap
