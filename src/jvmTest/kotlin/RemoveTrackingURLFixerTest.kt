af6cad001390c12c1c32f857794f88ff450216f9
Hakanai <hakanai@ephemeral.garden>
2024-08-08 13:10:03 +1000

    Fleshing out tracking param removal tests a little

--- src/jvmTest/kotlin/RemoveTrackingURLFixerTest.kt
+++ src/jvmTest/kotlin/RemoveTrackingURLFixerTest.kt
@@ -13,31 +13,53 @@ class RemoveTrackingURLFixerTest : FreeSpec({
         explanation.shouldBeNull()
     }
 
-    "removes utm_ parameters for all URLs" {
-        val originalUrl = URL("https", "example.com", "/", mapOf("utm_blah" to "blah", "foo" to "bar"))
-        val (newUrl, explanation) = RemoveTrackingURLFixer.fix(originalUrl)
-        newUrl.shouldBe(URL("https", "example.com", "/", mapOf("foo" to "bar")))
-        explanation.shouldBe("Removed tracking tokens from URL")
+    "removes utm_ parameters" - {
+        "for all URLs" {
+            val originalUrl = URL("https", "example.com", "/", mapOf("utm_blah" to "blah", "foo" to "bar"))
+            val (newUrl, explanation) = RemoveTrackingURLFixer.fix(originalUrl)
+            newUrl.shouldBe(URL("https", "example.com", "/", mapOf("foo" to "bar")))
+            explanation.shouldBe("Removed tracking tokens from URL")
+        }
+
+        "and removes the param map if it becomes empty" {
+            val originalUrl = URL("https", "example.com", "/", mapOf("utm_blah" to "blah"))
+            val (newUrl, explanation) = RemoveTrackingURLFixer.fix(originalUrl)
+            newUrl.shouldBe(URL("https", "example.com", "/", null))
+            explanation.shouldBe("Removed tracking tokens from URL")
+        }
     }
 
     // Since the list of tracking parameters is currently data-driven, testing just one site should suffice.
     // If we end up wanting to thoroughly test all of them, we'll want to make the test data-driven as well.
     "removes si parameter" - {
         "for YouTube URLs" {
-            val originalUrl = URL("https", "youtube.com", "/mYekiVRnM_A", mapOf("si" to "eKqSQe5NZLDjRq2e"))
+            val originalUrl = URL(
+                "https", "youtube.com", "/mYekiVRnM_A",
+                mapOf("si" to "eKqSQe5NZLDjRq2e", "foo" to "bar")
+            )
             val (newUrl, explanation) = RemoveTrackingURLFixer.fix(originalUrl)
-            newUrl.shouldBe(URL("https", "youtube.com", "/mYekiVRnM_A", null))
+            newUrl.shouldBe(URL("https", "youtube.com", "/mYekiVRnM_A", mapOf("foo" to "bar")))
             explanation.shouldBe("Removed tracking tokens from URL")
         }
 
         "for shortened YouTube URLs" {
+            val originalUrl = URL(
+                "https", "youtu.be", "/mYekiVRnM_A",
+                mapOf("si" to "eKqSQe5NZLDjRq2e", "foo" to "bar")
+            )
+            val (newUrl, explanation) = RemoveTrackingURLFixer.fix(originalUrl)
+            newUrl.shouldBe(URL("https", "youtu.be", "/mYekiVRnM_A", mapOf("foo" to "bar")))
+            explanation.shouldBe("Removed tracking tokens from URL")
+        }
+
+        "and removes the param map if it becomes empty" {
             val originalUrl = URL("https", "youtu.be", "/mYekiVRnM_A", mapOf("si" to "eKqSQe5NZLDjRq2e"))
             val (newUrl, explanation) = RemoveTrackingURLFixer.fix(originalUrl)
             newUrl.shouldBe(URL("https", "youtu.be", "/mYekiVRnM_A", null))
             explanation.shouldBe("Removed tracking tokens from URL")
         }
 
-        "but not for non-YouTube URLs" {
+        "but does not modify non-YouTube URLs" {
             val originalUrl = URL("https", "example.com", "/", mapOf("si" to "eKqSQe5NZLDjRq2e"))
             val (newUrl, explanation) = RemoveTrackingURLFixer.fix(originalUrl)
             newUrl.shouldBeSameInstanceAs(originalUrl)
